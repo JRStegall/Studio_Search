2cc8953f0217f300a8db2b30c49deb1d04ada3fb
Jeremy <jeremy.stegall77@gmail.com>
2020-11-16 15:16:31 -0500

    Update navbar to collapse

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,57 +1,39 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../index.css";
-import Navbar from 'react-bootstrap/Nav';
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 
-function Nav() {
-  return (
-      <Navbar className="navbar navbar-collapse navbar-expand-lg navbar-dark bg-dark">
+
+function Navigate(){
+    return(
         <div className="container">
-        <Link className="nav-link" to={"/home"}>
-                 Studio Search
-                </Link>
-          <Button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarResponsive"
-            aria-controls="navbarResponsive"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </Button>
-          <div className="collapse navbar-collapse" id="navbarResponsive">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item active">
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/recording"}>
-                 Recording Studios
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/rehearsal"}>
-                  Rehearsal Studios
-                </Link>
-              </li>
-              <li>
-              <div class="search-container">
-    <form action="/action_page.php">
-      <input type="text" placeholder="Search.." name="search"/>
-      <button type="submit">Submit</button>
-    </form>
-  </div>
-              </li>
-              
-            </ul>
-          </div>
-        </div>
-      </Navbar>
-   
-  );
+<Navbar bg="dark" variant="dark" expand="lg">
+  <Navbar.Brand href="./Home.js">Studio Search</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="/recording">Recording Studios</Nav.Link>
+      <Nav.Link href="/rehearsal">Rehearsal Studios</Nav.Link>
+      <Nav.Link href="/dance">Dance Studios</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
+</div>
+    )
 }
 
-export default Nav;
+export default Navigate;
