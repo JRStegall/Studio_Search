be2a47d2d8e304629a27c9907cd245d6a51e41ba
Brandon Hill <60332058+Bhill1293@users.noreply.github.com>
2020-11-17 19:21:03 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React,{/* { useState } */}from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Recording from './components/Recording';
-import Rehearsal from './components/Rehearsal';
-import Nav from './components/Nav';
+import Home from './Components/Home';
+import Recording from './Components/Recording.js';
+import Rehearsal from './Components/Rehearsal';
+import Nav from './Components/Nav';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Register from './Components/Register/Register';
+
 // import Form from 'react-bootstrap/Form';
 // import Row from 'react-bootstrap/Row';
 // import Col from 'react-bootstrap/Col';
@@ -27,7 +29,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/recording" component={Recording} />
           <Route path="/rehearsal" component={Rehearsal} />
-      
+          <Route path="/register" component={Register}/>
           <div className="container">
             <div className="row">
               <div className="col-lg-12">
