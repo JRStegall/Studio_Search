2f32f4255a834532195c20be2838ba3f10578814
Jeremy <jeremy.stegall77@gmail.com>
2020-11-21 11:15:22 -0500

    Create working search page

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,96 +1,98 @@
-let searchButton = document.getElementById('searchButton')
-let nutrition = [];
-searchButton.addEventListener('click', function () {
-    let studio = document.getElementById('Search').value
-    if (studio === '') {
-        alert("Please enter a search query...");
-    }
-    axios.get(`recording_studios.dbs`)
-        .then((data) => {
-            let results = document.getElementById('results')
-            results.innerHTML = ''
-            nutrition = data
-            for (i = 0; i < 5; i++) {
-                let resultsList = document.createElement('div')
-                resultsList.innerHTML = data.data.hints[i].food.label
-                results.appendChild(resultsList)
-                let nutritionInfo = document.createElement('div')
-                nutritionInfo.innerHTML = `
-            calories: ${data.data.hints[i].food.nutrients.ENERC_KCAL},
-            carbs: ${data.data.hints[i].food.nutrients.CHOCDF}g,
-            fat: ${data.data.hints[i].food.nutrients.FAT}g,
-            protein: ${data.data.hints[i].food.nutrients.PROCNT}g,
-            fiber: ${data.data.hints[i].food.nutrients.FIBTG}g
-            `
-                results.appendChild(nutritionInfo)
-                let logFood = document.createElement('button')
-                logFood.innerHTML = 'Log Food'
-                logFood.className = 'logFood'
-                logFood.id = i
-                results.appendChild(logFood)
-                // console.log(data.data.hints[i].food.nutrients)
-                logFood.addEventListener('click', function (event) {
-                    console.log(nutrition.data.hints[event.target.id])
-                    let selectedFood = nutrition.data.hints[event.target.id]
-                    console.log(typeof selectedFood.food.nutrients.FAT)
-                    $.post('/intake', {
-                        "food": `${selectedFood.food.label}`,
-                        "calories": `${selectedFood.food.nutrients.ENERC_KCAL}`,
-                        "carb_g": `${selectedFood.food.nutrients.CHOCDF}`,
-                        "fat_g": `${selectedFood.food.nutrients.FAT}`,
-                        "pro_g": `${selectedFood.food.nutrients.PROCNT}`,
-                        "fiber": `${selectedFood.food.nutrients.FIBTG}`,
-                    }, function (response) {
-                        console.log(response)
-                    })
-                    let newLog = document.createElement('div')
-                    newLog.innerHTML = `food: ${selectedFood.food.label} ,
-                    calories: ${selectedFood.food.nutrients.ENERC_KCAL},
-                    carb: ${selectedFood.food.nutrients.CHOCDF},
-                    fat: ${selectedFood.food.nutrients.FAT},
-                    protein: ${selectedFood.food.nutrients.PROCNT},
-                    fiber: ${selectedFood.food.nutrients.FIBTG}
-                    `
-                    let deleteButton = document.createElement('button')
-                    deleteButton.innerHTML = 'Delete Item'
-                    intakeLog.appendChild(newLog)
-                    intakeLog.appendChild(deleteButton)
-                    deleteButton.addEventListener('click', function(){
-                        console.log('Delete Button Works')
-                        intakeLog.removeChild(newLog)
-                        intakeLog.removeChild(deleteButton)
-                    })
-                })
-            }
-            document.getElementById('search').value = ''
-        })
-        .catch((error) => {
-            console.log('error')
-        })
-})
-let updateButton = document.getElementById('updateButton')
-updateButton.addEventListener('click', function () {
-    let age = document.getElementById('age').value
-    let height_in = document.getElementById('height_in').value
-    let weight_lbs = document.getElementById('weight_lbs').value
-    let gender = document.getElementById('gender').value
-    age = parseInt(age)
-    height_in = parseInt(height_in)
-    weight_lbs = parseInt(weight_lbs)
-    axios.put(`/userdata`, { "age": `${age}`, "height_in": `${height_in}`, "weight_lbs": `${weight_lbs}`, "gender": `${gender}` })
-        .then((data) => {
-            console.log(data)
-            console.log(age, height_in, weight_lbs, gender)
-            document.getElementById('age').value = ''
-            document.getElementById('height_in').value = ''
-            document.getElementById('weight_lbs').value = ''
-            document.getElementById('gender').value = ''
-            document.getElementById('userAge').innerHTML = `age: ${age}`
-            document.getElementById('userGender').innerHTML = `gender: ${gender}`
-            document.getElementById('userHeight').innerHTML = `height: ${height_in} in`
-            document.getElementById('userWeight').innerHTML = `weight: ${weight_lbs} lbs`
-        })
-        .catch((error) => {
-            console.log("somethin here ain't right")
-        })
-})
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Card from 'react-bootstrap/Card';
+import CardColumns from 'react-bootstrap/CardColumns';
+
+
+function Search() {
+  const [studios, setStudios] = useState(false)
+  const [search, setSearch] = useState(false)
+
+  function fetchStudios() {
+    console.log(search);
+    //replace route with set URL//
+    fetch(`http://localhost:4000/search=${search}`)
+      .then(res => res.json())
+      .then((results) => {
+        console.log(results);
+        setStudios([results]);
+        console.log(studios);
+      })
+  }
+
+  
+
+  function handleChange(event){
+    setSearch(event.target.value);
+  }
+
+  const items = []
+  if (studios) {
+    console.log(studios);
+    studios.forEach( (item, index) => {
+      console.log(item[index])
+
+      if(item[index]) {
+        items.push(
+        <Card key={item[index].id} style={{ width: '16rem' }}>
+          <Card.Img variant="top" src={item[index].image} />
+          <Card.Body>
+            <Card.Title>{item[index].name}</Card.Title>
+            <Card.Text>
+              {item[index].address}
+              <br/>
+              {item[index].city}, &nbsp;
+              {item[index].state}
+              <br/>
+              {item[index].zip_code}
+              <br/>
+            <a href={item[index].map} target="_blank" rel="noreferrer">Map</a>
+            <br />
+            <a href={item[index].website} target="_blank" rel="noreferrer">{item[index].website}</a>
+            <br/>
+    <a href='tel:{item.phone_number}'>{item[index].phone_number}</a>
+            </Card.Text>
+            
+          </Card.Body>
+        </Card>
+      )
+        }
+    })
+  }
+
+
+  return (
+    <div className="body">
+    <Container>
+      <Row>
+        <Col className="col">
+        <InputGroup className="mb-3 mt-4">
+          <FormControl
+            placeholder="Search for Studios..."
+            aria-label="Studio Search"
+            aria-describedby="basic-addon2"
+            onChange={handleChange}
+          />
+          <InputGroup.Append>
+            <Button onClick={fetchStudios} variant="btn btn-primary">Search</Button>
+          </InputGroup.Append>
+        </InputGroup>
+        </Col>
+      </Row>
+
+    <CardColumns>
+      {items}
+      </CardColumns>
+
+    </Container>
+    </div>
+  );
+}
+
+export default Search;
