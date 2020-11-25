bc85de60639ac00eb2907601898221df7c2bcd07
Jeremy <jeremy.stegall77@gmail.com>
2020-11-25 17:09:01 -0500

    Mod app.js

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ function App() {
 
 
   return (
+    <div className="App">
     <Router>
-      <div className="App">
         <Navigate />
         <Switch>
           <Route path="/recording" component={Recording} />
